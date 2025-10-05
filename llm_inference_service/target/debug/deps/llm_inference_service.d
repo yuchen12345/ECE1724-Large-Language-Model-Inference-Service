d586f3db40c604ca7451ed3a8e689a4183abc86b
OE Z <yuchenzoe.xu@mail.utoronto.ca>
2025-10-04 21:45:45 -0400

    Add initial Rust cargo project

---
+++ llm_inference_service/target/debug/deps/llm_inference_service.d
@@ -0,0 +1,5 @@
+C:\Users\yuche\OneDrive\桌面\2025\2025fall\1724Rust\ECE1779-Large-Language-Model-Inference-Service\llm_inference_service\target\debug\deps\llm_inference_service.d: src\main.rs
+
+C:\Users\yuche\OneDrive\桌面\2025\2025fall\1724Rust\ECE1779-Large-Language-Model-Inference-Service\llm_inference_service\target\debug\deps\llm_inference_service.exe: src\main.rs
+
+src\main.rs:
